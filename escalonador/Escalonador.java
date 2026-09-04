55736d5d75faec4f70bae16dd2cfa88468235e5a
juuh davies <juuh.davies12@gmail.com>
2026-09-04 12:45:46 -0300

    escalonador com métodos esqueleto + roundrobin

--- escalonador/Escalonador.java
+++ escalonador/Escalonador.java
@@ -1,5 +1,11 @@
 package escalonador;
 
-public class Escalonador {
-    
+import modelo.Processo;
+
+
+//padronizar métodos para entrada, seleção e verificação de processos pendentes
+public interface Escalonador {
+    void adicionarProcesso(Processo p); //adiciona processo à fila de prontos
+    Processo proximoProcesso(int tempoAtual); //seleciona o próximo processo a ser executado, atualiza o estado do processo e retorna o processo selecionado
+    boolean temProcessoPendente(); //boolean que indica se há processos pendentes na fila de prontos
 }
